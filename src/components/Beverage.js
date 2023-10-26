e537c8ac5d523d4e9ca14a2c5dfe6ed98020876a
gilles20393 <gilles1996@outlook.com>
2023-10-26 15:53:06 +0200

    vraag 3

--- src/components/Beverage.js
+++ src/components/Beverage.js
@@ -2,24 +2,29 @@ import { Section } from "./Section";
 import * as PropTypes from "prop-types";
 import { Col } from "react-bootstrap";
 import { MyCard } from "./MyCard";
-
-
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 function Beverage(props) {
     const { product } = props;
+    const [inStock, setInStock] = useState(product.inStock);
 
     const productStyle = {
-        color: product.inStock < 1 ? 'gray' : 'black'
+        color: inStock < 1 ? 'gray' : 'black'
     };
+
+    const handleOrderClick = () => {
+        setInStock(inStock - 1);
+    }
 
     return (
         <Col xs={6} sm={4} md={3} lg={2}>
-
             <MyCard title={product.name}>
                 <div style={productStyle}>
                     <div>{product.price} EUR</div>
+                    {inStock > 0 && <Button disabled={false} onClick={handleOrderClick}>Order</Button>}
                     <hr />
-                    <div><i>in stock: {product.inStock}</i></div>
+                    <div><i>in stock: {inStock}</i></div>
                 </div>
             </MyCard>
         </Col>
